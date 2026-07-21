06937de4def3e6adbf0b45584ca67a4d922258fb
baik100 <bcy0729@naver.com>
2026-07-21 11:04:49 +0900

    Add basic SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet-async";
+
 import Hero from './components/Hero'
 import About from './components/About'
 import Review from './components/Review'
@@ -7,14 +9,37 @@ import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="bg-black">
-      <Hero />
-      <About />
-      <Review />
-      <Service />
-      <Portfolio />
-      <Contact />
-    </div>
+    <>
+      <Helmet>
+        <title>슬로우바운드 | 브랜드가 기억되는 홈페이지 제작</title>
+
+        <meta
+          name="description"
+          content="브랜드가 기억되는 홈페이지를 제작합니다. 랜딩페이지, 기업 홈페이지, 웹디자인 전문."
+        />
+
+        <meta
+          name="keywords"
+          content="홈페이지 제작, 랜딩페이지, 웹디자인, UIUX, 슬로우바운드"
+        />
+
+        <meta property="og:title" content="슬로우바운드" />
+
+        <meta
+          property="og:description"
+          content="브랜드가 기억되는 홈페이지 제작"
+        />
+      </Helmet>
+
+      <div className="bg-black">
+        <Hero />
+        <About />
+        <Review />
+        <Service />
+        <Portfolio />
+        <Contact />
+      </div>
+    </>
   )
 }
 
